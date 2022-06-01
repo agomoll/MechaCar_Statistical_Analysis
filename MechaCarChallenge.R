b731b9de77ed41aa03f540d800d0905bac981880
Andrew Gomoll <andrewgomoll@gmail.com>
2022-06-01 16:56:59 -0700

    Adding summarry stats for coil psi bby manufacturing lot

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,24 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 
 
+# ----- Deliverable 2 -----
+# Creating visualizations for the Trip Analysis
+
+# Import Suspension_Coil.csv
+suspension_table <- read.csv("Suspension_Coil.csv")
+
+
+# Create summary table of PSI Stats
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI) )
+
+
+# group each manufacturing lot and display PSI stats
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI) )
+
+
+
+
+
+
+
+                      
